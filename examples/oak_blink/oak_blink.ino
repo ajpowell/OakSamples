704401ab073a98c125cd5644ec087ffc1eb1c386
Adrian Powell <apowell@Adrians-MBP.local>
2016-02-01 22:12:09 +0000

    Edit to oak_blink.ino

--- examples/oak_blink/oak_blink.ino
+++ examples/oak_blink/oak_blink.ino
@@ -8,16 +8,20 @@
 //
 // ver Date       Author       Comment
 // 1.0 30/01/2016 ajpowell     Initial version
+// 1.1 01/02/2016 ajpowell     Edited to use BUILTIN_LED
+
+// BUILTIN_LED is defined in oak/pins_arduino.h
+// static const uint8_t BUILTIN_LED = 1;
 
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(1, OUTPUT); //LED on Model A   
+  pinMode(BUILTIN_LED, OUTPUT); //LED on Model A   
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(BUILTIN_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
   Particle.delay(100);               // wait for a tenth second
-  digitalWrite(1, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(BUILTIN_LED, LOW);    // turn the LED off by making the voltage LOW
   Particle.delay(4000);               // wait for a second
 }
