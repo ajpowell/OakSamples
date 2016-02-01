04e45024c933644b10fadcf455c0324d750a5e6c
Adrian Powell <apowell@Adrians-MBP.local>
2016-02-01 21:53:05 +0000

    Edit to oak_digiLED.ino

--- examples/oak_digiLED/oak_digiLED.ino
+++ examples/oak_digiLED/oak_digiLED.ino
@@ -4,6 +4,7 @@
 // ver Date       Author       Comment
 // 1.0 30/01/2016 ajpowell     Updated for Digistump Oak - using Particle.delay() 
 //                             rather than delay()
+// 1.1 01/02/2016 ajpowell     Added Serial output for debugging
 
 #include <Adafruit_NeoPixel.h>
  
@@ -21,39 +22,48 @@ int delayval = 500; // delay for half a second
  
 void setup() 
 {
+  Serial.begin(74880);
   pixels.begin(); // This initializes the NeoPixel library.
   pixels.show(); // Initialize all pixels to 'off'
+  Serial.println("\n oak_digiLED\n");
 }
  
 void loop()
 {
   // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
   // in Red,Green,Blue order
- 
+
+  Serial.println("White...");
   pixels.setPixelColor(0, pixels.Color(255, 255, 255)); //white
   pixels.show(); // This sends the updated pixel color to the hardware.
   Particle.delay(delayval); // Delay for a period of time (in milliseconds).
- 
+
+  Serial.println("Cyan...");
   pixels.setPixelColor(0, pixels.Color(0, 255, 255)); //cyan
   pixels.show();
   Particle.delay(delayval);
- 
+
+  Serial.println("Violet...");
   pixels.setPixelColor(0, pixels.Color(255, 0, 255)); //violet
   pixels.show();
   Particle.delay(delayval);
- 
+
+  Serial.println("Yellow...");
   pixels.setPixelColor(0, pixels.Color(255, 255, 0)); //yellow
   pixels.show();
   Particle.delay(delayval);
- 
+
+  Serial.println("Red...");
   pixels.setPixelColor(0, pixels.Color(255, 0, 0)); //red
   pixels.show();
   Particle.delay(delayval);
- 
+
+  Serial.println("Green...");
   pixels.setPixelColor(0, pixels.Color(0, 255, 0)); //green
   pixels.show();
   Particle.delay(delayval);
- 
+
+  Serial.println("Blue...");
   pixels.setPixelColor(0, pixels.Color(0, 0, 255)); //blue
   pixels.show();
   Particle.delay(delayval);
